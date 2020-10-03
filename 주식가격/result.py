27234aeb373a36e7b8a1565776f64d3896839d1a
orlando <59018852+ingkoon@users.noreply.github.com>
2020-10-03 14:54:11 +0900

    주식가격 알고리즘

---
+++ 주식가격/result.py
@@ -0,0 +1,12 @@
+def solution(prices):
+    answer = []
+    for i in range(len(prices)):
+        count = 0
+        for j in range(i+1,len(prices)):
+            if prices[i] <= prices[j]:
+                count += 1
+        answer.append(count)
+    return answer
+
+prices = [1,2,3,2,4,5,3,2,4,5]
+print(solution(prices))

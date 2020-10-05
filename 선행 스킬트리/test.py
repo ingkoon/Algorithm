1c88b62efdedb04d731ea142ded3c3d7574d628a
Orlando <59018852+ingkoon@users.noreply.github.com>
2020-10-05 23:58:33 +0900

    #2

--- 선행 스킬트리/test.py
+++ 선행 스킬트리/test.py
@@ -4,8 +4,15 @@ def solution(skill, skill_trees):
     for i in range (len(skill)):
         eval1.append([skill[i],i])
 
+    temp = []
+    eval2 = []
+    for j in range (len(skill_trees)):
+        for k in range (len(skill_trees[j])):
+            temp.append([skill_trees[j][k],k])
+        eval2.append(temp)
+        temp = []
+    
     return answer
-
 skill = "CBD"
 
 skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]

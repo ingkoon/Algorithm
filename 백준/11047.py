01d799e062635ac70189f5b8eeebf7f44b164d8b
orlando <59018852+ingkoon@users.noreply.github.com>
2021-01-12 15:14:14 +0900

    11047번

---
+++ 백준/11047.py
@@ -0,0 +1,19 @@
+a, b = (map(int, input().split(' ')))
+print(a)
+print(b)
+arr = []
+
+for i in range(a):
+    arr.append(int(input()))
+    
+arr.sort(reverse = True)
+
+cnt = 0
+
+for j in range(len(arr)):
+
+    cnt += int(b/arr[j])
+    b = b%arr[j]
+
+print(cnt)
+    

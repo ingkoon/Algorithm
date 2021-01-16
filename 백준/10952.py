90492a4ece8bf0deafc12d7e2d08ba0d868297a4
orlando <59018852+ingkoon@users.noreply.github.com>
2021-01-16 22:16:41 +0900

    1110

--- 백준/10952.py
+++
@@ -1,9 +0,0 @@
-a, b = map(int, input().split(' '))
-
-while True:
-    if ((a == 0) and (b == 0)) and (0<a,b<10):
-        break;
-    else:
-        print(a+b)
-        a, b = map(int, input().split(' '))
-    

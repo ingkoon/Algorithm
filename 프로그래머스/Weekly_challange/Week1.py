0281d7019f1cb7f415ea925751292aa91cff0402
orlando <59018852+ingkoon@users.noreply.github.com>
2021-08-06 21:34:30 +0900

    biggest number(1)

--- 프로그래머스/Weekly_challange/Week1.py
+++ 프로그래머스/Weekly_challange/Week1.py
@@ -1,5 +1,6 @@
 def solution(price, money, count):
-    tmp = 0
+    # 임시 변수 할당
+    tmp = 0    
     for i in range(1,count+1):
         tmp += price * i
     answer = tmp - money
---
+++ 프로그래머스/큰 수 만들기.py
@@ -0,0 +1,8 @@
+#k개의 수를 어떻게 빼는가? => 
+def solution(number, k):
+    
+    stk= []
+    for i in number
+        print(i)    
+    answer = ''
+    return answer

347cbba0f8ac716ab9ee31f5fadc72371c8c7df3
Orlando <59018852+ingkoon@users.noreply.github.com>
2021-08-09 10:41:59 +0900

    [2021-08-09] knight(1)

--- 이것이 코딩 테스트다/시각.py
+++ 이것이 코딩 테스트다/시각.py
@@ -1,14 +1,19 @@
 import sys
 
+# 정수타입으로 시각 입력
 n = int(sys.stdin.readline())
 
+# 3이 카운트 되기 위한 변수 지정
 cnt = 0
 
+# 시, 분, 초를 계산하기 위하여 3중 반복문으로 구성
 for i in range(n+1):
     for j in range(60):
         for k in range(60):
+            # 시, 분, 초를 각각 문자열로 변경하여 3이 포함되는지 확인
             t = str(i) + str(j) + str(k)
             if "3" in t:
                 cnt += 1
 
+# 출력
 print(cnt)

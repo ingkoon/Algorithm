8885fb27cc826d658a5181474bdc49afe9e0b994
orlando <59018852+ingkoon@users.noreply.github.com>
2021-11-30 23:21:49 +0900

    [2021-11-30] 아이디추천(12)

---
+++ 신규 아이디 추천/test.py
@@ -0,0 +1,17 @@
+def solution(new_id):
+    answer = ''
+    if new_id is None:
+        answer = 'a'
+        return answer
+    
+    new_id = new_id.lower()
+    
+    for i in range(len(new_id)):
+        
+        
+    
+    
+    
+    
+    
+    return answer

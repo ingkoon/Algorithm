06661d3db7a555c735c1aff15decddd81b72d79d
orlando <59018852+ingkoon@users.noreply.github.com>
2022-03-01 23:46:17 +0900

    [2022-03-01] 도메인

--- 동적 프로그래밍/RGB거리/test.py
+++ 동적 프로그래밍/RGB거리/test.py
@@ -24,13 +24,10 @@ n = int(sys.stdin.readline())
 
 rgb = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 
-print(rgb)
 
 for i in range(1, n):
     rgb[i][0] = min(rgb[i-1][1], rgb[i-1][2]) + rgb[i][0]
     rgb[i][1] = min(rgb[i-1][0], rgb[i-1][2]) + rgb[i][1]
     rgb[i][2] = min(rgb[i-1][0], rgb[i-1][1]) + rgb[i][2]
-    print(rgb[i][0], rgb[i][1], rgb[i][2])
 
-print(rgb)
 print(min(rgb[n-1]))

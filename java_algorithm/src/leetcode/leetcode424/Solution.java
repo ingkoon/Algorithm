d0ba1519856e57d61145900d1efad007e2d23db0
ingkoon <ing97220@naver.com>
2026-06-15 11:17:23 +0900

    [LeetCode-424] fix : Longest Repeating Character Replacement

--- java_algorithm/src/leetcode/leetcode424/Solution.java
+++ java_algorithm/src/leetcode/leetcode424/Solution.java
@@ -1,40 +1,20 @@
 package leetcode.leetcode424;
 
-import java.util.Map;
-import java.util.HashMap;
-
 public class Solution {
     public int characterReplacement(String s, int k) {
-        if (s.length() == 1) {
-            return s.length();
-        }
-        int start = 0;
-        int end = 1;
+        int[] count = new int[26];
+        int maxCount = 0, maxLength = 0, start = 0;
 
-        Map<Character, Integer> map = new HashMap<>();
-        map.put(s.charAt(start), 1);
-        int max = 1;
-        int size = 1;
-        int result = 1;
+        for (int end = 0; end < s.length(); end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
 
-        while (end < s.length() && start < end) {
-            char endChar = s.charAt(end);
-            int endCnt = map.getOrDefault(endChar, 0) + 1;
-            map.put(endChar, endCnt);
-            max = Math.max(max, endCnt);
-            size++;
-
-            while ((size - max) > k) {
-                char startChar = s.charAt(start);
-                int startCnt = map.get(startChar);
-                map.put(startChar, startCnt - 1);
-                size--;
-                start++;
+            while (end - start + 1 - maxCount > k) {
+                count[s.charAt(start++) - 'A']--;
             }
 
-            result = Math.max(result, size);
-            end++;
+            maxLength = Math.max(maxLength, end - start + 1);
         }
-        return result;
+
+        return maxLength;
     }
 }

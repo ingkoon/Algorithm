e1dc5f5a67f708dd961560cad6a85fc00842df97
ingkoon <dlswotmd@gmail.com>
2026-07-02 16:05:27 +0900

    [LeetCode-72] done : Edit Distance

---
+++ java_algorithm/src/leetcode/leetcode72/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.leetcode72;
+
+public class Solution {
+    public int minDistance(String word1, String word2) {
+        int row = word1.length();
+        int column = word2.length();
+
+        int[][] dp = new int[row + 1][column + 1];
+
+        for (int i = 0; i <= row; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 0; i <= column; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= column; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[row][column];
+    }
+}

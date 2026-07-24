4ef69a691cdcc96103a27fa47f2894c251332e12
ingkoon <dlswotmd@gmail.com>
2026-07-24 14:59:51 +0900

    [Programmers-43164] fix : 여행경로

--- java_algorithm/src/programmers/programmers43164/Solution.java
+++ java_algorithm/src/programmers/programmers43164/Solution.java
@@ -2,12 +2,13 @@ package programmers.programmers43164;
 
 import java.util.*;
 
-public class Solution {
+class Solution {
     static boolean[] visited;
-    static List<String> allRoutes = new ArrayList<>();
+    static List<String> routeList;
 
     public String[] solution(String[][] tickets) {
         visited = new boolean[tickets.length];
+        routeList = new ArrayList<>();
 
         Arrays.sort(tickets, (o1, o2) -> {
             if (o1[0].equals(o2[0])) {
@@ -16,23 +17,30 @@ public class Solution {
             return o1[0].compareTo(o2[0]);
         });
 
-        dfs("ICN", "ICN", tickets, 0);
+        routeList.add("ICN");
+        dfs("ICN", routeList, tickets, 0);
 
-        return allRoutes.get(0).split(" ");
+        return routeList.toArray(new String[0]);
     }
 
-    public void dfs(String current, String route, String[][] tickets, int count) {
+    private boolean dfs(String current, List<String> route, String[][] tickets, int count) {
+        // 모든 티켓을 전부 사용한 경우 (성공)
         if (count == tickets.length) {
-            allRoutes.add(route);
-            return;
+            return true;
         }
 
         for (int i = 0; i < tickets.length; i++) {
             if (!visited[i] && tickets[i][0].equals(current)) {
                 visited[i] = true;
-                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, count + 1);
+                route.add(tickets[i][1]);
+                if (dfs(tickets[i][1], route, tickets, count + 1)) {
+                    return true;
+                }
+                route.remove(route.size() - 1);
                 visited[i] = false;
             }
         }
+
+        return false;
     }
 }
